af0af846b8105d16f58670832d131aa68472395a
sumets <46512831+sumets@users.noreply.github.com>
2024-07-17 17:29:26 +0700

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,9 @@
+# # Databricks notebook source
+!pip install sqlparse
+!pip install azure-cosmos
+!pip install adal
+!pip install databricks-sql-connector==2.7.0
+#DATAX
+!pip install confluent-kafka==1.8.2
+!pip install pythainlp
+!pip install jellyfish
