202126a9484d8ec61528a6c9bebdbbec4cb6f738
sumets <46512831+sumets@users.noreply.github.com>
2024-07-17 12:46:44 +0700

    Update func.py

--- func.py
+++ func.py
@@ -1,12 +1,12 @@
-# Databricks notebook source
-!pip install sqlparse
-!pip install azure-cosmos
-!pip install adal
-!pip install databricks-sql-connector==2.7.0
-#DATAX
-!pip install confluent-kafka==1.8.2
-!pip install pythainlp
-!pip install jellyfish
+# # Databricks notebook source
+# !pip install sqlparse
+# !pip install azure-cosmos
+# !pip install adal
+# !pip install databricks-sql-connector==2.7.0
+# #DATAX
+# !pip install confluent-kafka==1.8.2
+# !pip install pythainlp
+# !pip install jellyfish
 
 # COMMAND ----------
 
